bf159afce96d1e0f918d4f95d334d304555096b4
JoseHerz <102639984+JoseHerz@users.noreply.github.com>
2023-12-02 16:09:13 -0600

    Cambios Guardados

--- Software_Medico/Vistas/Aux_Inventario.Designer.cs
+++ Software_Medico/Vistas/Aux_Inventario.Designer.cs
@@ -28,20 +28,337 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Pnl_Formulario = new System.Windows.Forms.Panel();
+            this.Txt_EnfCronica = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.Txt_Cantidad = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.Txt_Precio = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Cmb_TipoArticulo = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Btn_Cancelar = new System.Windows.Forms.Button();
+            this.Btn_Guardar = new System.Windows.Forms.Button();
+            this.Txt_Nombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Pnl_Barra = new System.Windows.Forms.Panel();
+            this.Ptb_Min = new System.Windows.Forms.PictureBox();
+            this.Ptb_Cerrar = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Dtp_Vencimiento = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Cmb_Categoria = new System.Windows.Forms.ComboBox();
+            this.Pnl_Formulario.SuspendLayout();
+            this.Pnl_Barra.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ptb_Min)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ptb_Cerrar)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Pnl_Formulario
+            // 
+            this.Pnl_Formulario.BackColor = System.Drawing.SystemColors.Control;
+            this.Pnl_Formulario.Controls.Add(this.Cmb_Categoria);
+            this.Pnl_Formulario.Controls.Add(this.label3);
+            this.Pnl_Formulario.Controls.Add(this.Dtp_Vencimiento);
+            this.Pnl_Formulario.Controls.Add(this.Txt_EnfCronica);
+            this.Pnl_Formulario.Controls.Add(this.label15);
+            this.Pnl_Formulario.Controls.Add(this.Txt_Cantidad);
+            this.Pnl_Formulario.Controls.Add(this.label13);
+            this.Pnl_Formulario.Controls.Add(this.label12);
+            this.Pnl_Formulario.Controls.Add(this.label11);
+            this.Pnl_Formulario.Controls.Add(this.Txt_Precio);
+            this.Pnl_Formulario.Controls.Add(this.label9);
+            this.Pnl_Formulario.Controls.Add(this.label8);
+            this.Pnl_Formulario.Controls.Add(this.Cmb_TipoArticulo);
+            this.Pnl_Formulario.Controls.Add(this.label5);
+            this.Pnl_Formulario.Controls.Add(this.Btn_Cancelar);
+            this.Pnl_Formulario.Controls.Add(this.Btn_Guardar);
+            this.Pnl_Formulario.Controls.Add(this.Txt_Nombre);
+            this.Pnl_Formulario.Controls.Add(this.label2);
+            this.Pnl_Formulario.Location = new System.Drawing.Point(0, 40);
+            this.Pnl_Formulario.Name = "Pnl_Formulario";
+            this.Pnl_Formulario.Size = new System.Drawing.Size(537, 482);
+            this.Pnl_Formulario.TabIndex = 10;
+            // 
+            // Txt_EnfCronica
+            // 
+            this.Txt_EnfCronica.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_EnfCronica.Location = new System.Drawing.Point(229, 326);
+            this.Txt_EnfCronica.Name = "Txt_EnfCronica";
+            this.Txt_EnfCronica.Size = new System.Drawing.Size(205, 30);
+            this.Txt_EnfCronica.TabIndex = 47;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(28, 329);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(201, 25);
+            this.label15.TabIndex = 46;
+            this.label15.Text = "Medida por Unidad:";
+            // 
+            // Txt_Cantidad
+            // 
+            this.Txt_Cantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Cantidad.Location = new System.Drawing.Point(139, 115);
+            this.Txt_Cantidad.Name = "Txt_Cantidad";
+            this.Txt_Cantidad.Size = new System.Drawing.Size(84, 30);
+            this.Txt_Cantidad.TabIndex = 43;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(27, 115);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(106, 25);
+            this.label13.TabIndex = 42;
+            this.label13.Text = "Cantidad:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(242, 118);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(113, 25);
+            this.label12.TabIndex = 35;
+            this.label12.Text = "Categoria:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(27, 225);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(241, 29);
+            this.label11.TabIndex = 33;
+            this.label11.Text = "Datos de Consumo:";
+            // 
+            // Txt_Precio
+            // 
+            this.Txt_Precio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Precio.Location = new System.Drawing.Point(113, 161);
+            this.Txt_Precio.Name = "Txt_Precio";
+            this.Txt_Precio.Size = new System.Drawing.Size(140, 30);
+            this.Txt_Precio.TabIndex = 30;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(27, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 25);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "Precio:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(27, 379);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(205, 25);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Tipo de Regulacion:";
+            // 
+            // Cmb_TipoArticulo
+            // 
+            this.Cmb_TipoArticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cmb_TipoArticulo.FormattingEnabled = true;
+            this.Cmb_TipoArticulo.Location = new System.Drawing.Point(238, 377);
+            this.Cmb_TipoArticulo.Name = "Cmb_TipoArticulo";
+            this.Cmb_TipoArticulo.Size = new System.Drawing.Size(187, 33);
+            this.Cmb_TipoArticulo.TabIndex = 27;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(243, 29);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Datos del Producto:";
+            // 
+            // Btn_Cancelar
+            // 
+            this.Btn_Cancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_Cancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(54)))), ((int)(((byte)(75)))));
+            this.Btn_Cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Cancelar.ForeColor = System.Drawing.SystemColors.Control;
+            this.Btn_Cancelar.Location = new System.Drawing.Point(-114, 471);
+            this.Btn_Cancelar.Name = "Btn_Cancelar";
+            this.Btn_Cancelar.Size = new System.Drawing.Size(154, 50);
+            this.Btn_Cancelar.TabIndex = 19;
+            this.Btn_Cancelar.Text = "Cancelar";
+            this.Btn_Cancelar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_Guardar
+            // 
+            this.Btn_Guardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_Guardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(54)))), ((int)(((byte)(75)))));
+            this.Btn_Guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Guardar.ForeColor = System.Drawing.SystemColors.Control;
+            this.Btn_Guardar.Location = new System.Drawing.Point(-274, 471);
+            this.Btn_Guardar.Name = "Btn_Guardar";
+            this.Btn_Guardar.Size = new System.Drawing.Size(154, 50);
+            this.Btn_Guardar.TabIndex = 18;
+            this.Btn_Guardar.Text = "Guardar";
+            this.Btn_Guardar.UseVisualStyleBackColor = false;
+            // 
+            // Txt_Nombre
+            // 
+            this.Txt_Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Nombre.Location = new System.Drawing.Point(219, 70);
+            this.Txt_Nombre.Name = "Txt_Nombre";
+            this.Txt_Nombre.Size = new System.Drawing.Size(329, 30);
+            this.Txt_Nombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre Producto:";
+            // 
+            // Pnl_Barra
+            // 
+            this.Pnl_Barra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(182)))));
+            this.Pnl_Barra.Controls.Add(this.Ptb_Min);
+            this.Pnl_Barra.Controls.Add(this.Ptb_Cerrar);
+            this.Pnl_Barra.Controls.Add(this.label1);
+            this.Pnl_Barra.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Pnl_Barra.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_Barra.Name = "Pnl_Barra";
+            this.Pnl_Barra.Size = new System.Drawing.Size(592, 40);
+            this.Pnl_Barra.TabIndex = 9;
+            // 
+            // Ptb_Min
+            // 
+            this.Ptb_Min.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Ptb_Min.BackColor = System.Drawing.Color.Transparent;
+            this.Ptb_Min.Image = global::Software_Medico.Properties.Resources.minimizar__3___1_;
+            this.Ptb_Min.Location = new System.Drawing.Point(492, 3);
+            this.Ptb_Min.Name = "Ptb_Min";
+            this.Ptb_Min.Size = new System.Drawing.Size(45, 40);
+            this.Ptb_Min.TabIndex = 3;
+            this.Ptb_Min.TabStop = false;
+            // 
+            // Ptb_Cerrar
+            // 
+            this.Ptb_Cerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Ptb_Cerrar.BackColor = System.Drawing.Color.Transparent;
+            this.Ptb_Cerrar.Image = global::Software_Medico.Properties.Resources.cerrar_sesion__1_;
+            this.Ptb_Cerrar.Location = new System.Drawing.Point(543, 3);
+            this.Ptb_Cerrar.Name = "Ptb_Cerrar";
+            this.Ptb_Cerrar.Size = new System.Drawing.Size(45, 40);
+            this.Ptb_Cerrar.TabIndex = 1;
+            this.Ptb_Cerrar.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Agregar Articulo";
+            // 
+            // Dtp_Vencimiento
+            // 
+            this.Dtp_Vencimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dtp_Vencimiento.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.Dtp_Vencimiento.Location = new System.Drawing.Point(268, 275);
+            this.Dtp_Vencimiento.Name = "Dtp_Vencimiento";
+            this.Dtp_Vencimiento.Size = new System.Drawing.Size(166, 30);
+            this.Dtp_Vencimiento.TabIndex = 55;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 25);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Fecha de Vencimiento:";
+            // 
+            // Cmb_Categoria
+            // 
+            this.Cmb_Categoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cmb_Categoria.FormattingEnabled = true;
+            this.Cmb_Categoria.Location = new System.Drawing.Point(361, 115);
+            this.Cmb_Categoria.Name = "Cmb_Categoria";
+            this.Cmb_Categoria.Size = new System.Drawing.Size(187, 33);
+            this.Cmb_Categoria.TabIndex = 57;
             // 
             // Aux_Inventario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(592, 635);
+            this.Controls.Add(this.Pnl_Formulario);
+            this.Controls.Add(this.Pnl_Barra);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Aux_Inventario";
             this.Text = "Aux_Inventario";
+            this.Pnl_Formulario.ResumeLayout(false);
+            this.Pnl_Formulario.PerformLayout();
+            this.Pnl_Barra.ResumeLayout(false);
+            this.Pnl_Barra.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ptb_Min)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ptb_Cerrar)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Pnl_Formulario;
+        private System.Windows.Forms.TextBox Txt_EnfCronica;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox Txt_Cantidad;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox Txt_Precio;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox Cmb_TipoArticulo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Btn_Cancelar;
+        private System.Windows.Forms.Button Btn_Guardar;
+        private System.Windows.Forms.TextBox Txt_Nombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel Pnl_Barra;
+        private System.Windows.Forms.PictureBox Ptb_Min;
+        private System.Windows.Forms.PictureBox Ptb_Cerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker Dtp_Vencimiento;
+        private System.Windows.Forms.ComboBox Cmb_Categoria;
     }
 }
